94dad50f5415be4cac715cc9e9a69202b2af9962
pvasbob <pvasbob@gmail.com>
2026-04-18 17:51:48 -0400

    Index Buffer

--- HZL-OpenGL/src/Application.cpp
+++ HZL-OpenGL/src/Application.cpp
@@ -122,21 +122,34 @@ int main(void)
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
 
-	float positions[6] = {
-		-0.5f, -0.5f,
-		 0.0f,  0.5f,
-		 0.5f, -0.5f
+	float positions[] = {
+		-0.5f, -0.5f,	// 0
+		 0.5f, -0.5f,	// 1	
+		 0.5f,  0.5f,	// 2
+		-0.5f,  0.5f	// 3
 	};
 
-	unsigned int buffer;
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	unsigned int buffer;	// has to be unsigned
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 	
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 2, 0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	unsigned int ibo;		// has to be unsigned
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+	
+
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 	std::cout << "VERTEX" << std::endl;
@@ -154,7 +167,10 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+		//glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)(3*sizeof(unsigned int)));
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
