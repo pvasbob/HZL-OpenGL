321a7c2a9fcd5a5024d3bd56becd93cd6c07255d
pvasbob <pvasbob@gmail.com>
2026-04-26 15:41:55 -0400

    Use imgui in this project

--- HZL-OpenGL/src/Application.cpp
+++ HZL-OpenGL/src/Application.cpp
@@ -81,9 +81,11 @@ int main(void)
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
 		// moving cameras 100 unit to the right, which is equal to moving object to left 100 units.
 		glm::mat4 view  = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0,0));
-		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
-		// the multiply order is pvm becasue of column major in opengl
-		glm::mat4 mvp   = proj * view * model;
+
+		// moved into while loop
+		//glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+		//// the multiply order is pvm becasue of column major in opengl
+		//glm::mat4 mvp   = proj * view * model;
 
 		//position matrix in lines 
 		//glm::vec4 vp1(100.0f, 100.0f, 0.0f, 1.0f);
@@ -107,8 +109,6 @@ int main(void)
 		shader.Bind();
 
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-		shader.SetUniformMat4f("u_MVP", mvp);
-
 
 		Texture texture("res/textures/ChernoLogo.png");
 		//Texture texture("res/textures/youyou.jpg");
@@ -128,12 +128,7 @@ int main(void)
 		ImGui::StyleColorsDark();
 
 
-		bool show_demo_window = true;
-		bool show_another_window = false;
-		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
- 
-
+		glm::vec3 translation(200, 200, 0);
 
 		float r = 0.0f;
 		float increment = 0.01f;
@@ -145,10 +140,18 @@ int main(void)
 
 			ImGui_ImplGlfwGL3_NewFrame();
 
+			glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+
+			glm::mat4 mvp   = proj * view * model;
+
+
+
 
 			GLCall(shader.Bind());
 
 			GLCall(shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f));	// does not take shaderID as argument.  
+			shader.SetUniformMat4f("u_MVP", mvp);
+
 
 			renderer.Draw(va, ib, shader);
 
@@ -160,20 +163,9 @@ int main(void)
 			r += increment;
 
 			{
-				static float f = 0.0f;
-				static int counter = 0;
-				ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
-				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
-				ImGui::Checkbox("Another Window", &show_another_window);
-
-				if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-					counter++;
-				ImGui::SameLine();
-				ImGui::Text("counter = %d", counter);
-
+				// the 0.0f ~ 960.0f to be the same as that in the proj matrix? 
+				// here &translation.x is the pointer to translation as well, which should be a pointer to translation. 
+				ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
 				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 			}
 
