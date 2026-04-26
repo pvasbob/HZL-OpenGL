d3e8f46cfbaf327fbb4bb3a30d43cb25be09588d
pvasbob <pvasbob@gmail.com>
2026-04-26 14:37:02 -0400

    model view proj matrices

--- HZL-OpenGL/src/Application.cpp
+++ HZL-OpenGL/src/Application.cpp
@@ -75,19 +75,25 @@ int main(void)
 
 		IndexBuffer ib(indices, 6);
 
-		//glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-		glm::mat4 proj = glm::perspective(glm::radians(45.0f), 960.0f / 540.0f, 0.1f, 100.0f);
-		glm::vec4 vp1(100.0f, 100.0f, 0.0f, 1.0f);
-		glm::vec4 vp2(200.0f, 100.0f, 0.0f, 1.0f);
-		glm::vec4 vp3(200.0f, 200.0f, 0.0f, 1.0f);
-		glm::vec4 vp4(100.0f, 200.0f, 0.0f, 1.0f);
+		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+		// moving cameras 100 unit to the right, which is equal to moving object to left 100 units.
+		glm::mat4 view  = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0,0));
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+		// the multiply order is pvm becasue of column major in opengl
+		glm::mat4 mvp   = proj * view * model;
+
+		//position matrix in lines 
+		//glm::vec4 vp1(100.0f, 100.0f, 0.0f, 1.0f);
+		//glm::vec4 vp2(200.0f, 100.0f, 0.0f, 1.0f);
+		//glm::vec4 vp3(200.0f, 200.0f, 0.0f, 1.0f);
+		//glm::vec4 vp4(100.0f, 200.0f, 0.0f, 1.0f);
 
 		// show the conversion result line by line from the vertex position to the -1.0 ~ 1.0 
 		// not gonna be red in  later.		#
-		glm::vec4 result1 = proj * vp1;
-		glm::vec4 result2 = proj * vp2;
-		glm::vec4 result3 = proj * vp3;
-		glm::vec4 result4 = proj * vp4;
+		//glm::vec4 result1 = proj * vp1;
+		//glm::vec4 result2 = proj * vp2;
+		//glm::vec4 result3 = proj * vp3;
+		//glm::vec4 result4 = proj * vp4;
 
 
 		//glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -98,7 +104,7 @@ int main(void)
 		shader.Bind();
 
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-		shader.SetUniformMat4f("u_MVP", proj);
+		shader.SetUniformMat4f("u_MVP", mvp);
 
 
 		Texture texture("res/textures/ChernoLogo.png");
